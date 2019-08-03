f5df57b2b5d704ce5c68be2ad926e80d4e3dcb18
Pavel V. Dimens <pdimens@live.com>
2019-08-03 12:02:46 -0500

    working genepop reader

--- src/ReadGenepop.jl
+++ src/ReadGenepop.jl
@@ -1,3 +1,5 @@
+#! /usr/bin/env julia
+
 mutable struct PopObj
     ind::Array{String,1}
     popid::Array{Union{Int64,String},1}
@@ -8,8 +10,8 @@ mutable struct PopObj
     yloc::Array{Union{Int64,Float64},1}
 end
 
-function readgenepop(infile::String, ploidy::Int64 = 2)
-    gpop = split(open(readlines,infile)[2:end],"POP");
+function readgenepop(infile::String; ploidy::Int64 = 2)
+    gpop = split(open(readlines,infile)[2:end], "POP")
     d = Dict()
     locinames = gpop[1]
     popid = []
@@ -17,8 +19,8 @@ function readgenepop(infile::String, ploidy::Int64 = 2)
     for i in 2:length(gpop)
         append!(popid, fill(i-1,length(gpop[i])))
         for j in 1:length(gpop[i])
-            push!(indnames, split( gpop[i][j], r"\,|\t")[1] )
-            d[ indnames[j] ] = split( strip(gpop[i][j]), r"\s|\t" )[2:end] |> Array{String,1}
+            push!(indnames, split( strip(gpop[i][j]), r"\,|\t")[1] )
+            d[ last(indnames) ] = split( strip(gpop[i][j]), r"\s|\t" )[2:end] |> Array{String,1}
        end
     end
     return PopObj(indnames,
@@ -30,3 +32,7 @@ function readgenepop(infile::String, ploidy::Int64 = 2)
                   fill( 0, length(indnames) )
                   )
 end
+
+#=  TESTING
+@time test = readgenepop("test.gen", ploidy = 2)
+=#
